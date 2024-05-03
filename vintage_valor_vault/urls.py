73acfe5c2dd8bab38dc621c69199d5699e6d28f4
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-05-03 12:23:57 +0200

    Add home app and templates
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- vintage_valor_vault/urls.py
+++ vintage_valor_vault/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for vintage_valor_vault project.
+URL configuration for boutique_ado project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path('', include('home.urls')),
 ]
